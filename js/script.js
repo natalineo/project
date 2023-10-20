433dbf84e0444b4b23ce6e18b2cc7e4e170f051e
Natali <natalimaleta@gmail.com>
2023-10-20 13:48:42 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+console.log(obj.name + ' ' + obj.age);
+let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+console.log(arr[1]);
